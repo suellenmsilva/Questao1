dd723e4fb46538fe8e0ca715897933b627ca980f
suellencmsilva@gmail.com <suellencmsilva@gmail.com>
2020-06-05 11:00:57 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using static System.Int32;
 using static System.Console;
 using System.Collections.Generic;
@@ -35,6 +36,56 @@ namespace Questao1
             for (int i = 0; i < listaTemp.Count; i++)
             {
                 if (listaTemp[i] + listaTemp[ i + 1] == target)
+                {
+                    result.Add(i);
+                    result.Add(i + 1);
+                    Write($"[{i},{i + 1}]");
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
+=======
+﻿using static System.Int32;
+using static System.Console;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace Questao1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Target();
+        }
+        static void Alvo()
+        {
+            var listTemp = new List<int>();
+            var result = new List<int>();
+
+            Write("Alvo: ");
+            var target = Parse(ReadLine());
+
+            var num = new List<int>();
+            WriteLine("Digite o tamanho do array: ");
+            var total = Parse(ReadLine());
+
+            WriteLine("Digite os números: ");
+            for (var i = 0; i < total; i++)
+            {
+                var numero = Parse(ReadLine());
+                listTemp.Add(numero);
+            }
+            listTemp.Sort();
+
+            for (int i = 0; i < listTemp.Count; i++)
+            {
+                if (listTemp[i] + listTemp[ i + 1] == target)
                 {
                     result.Add(i);
                     result.Add(i + 1);
